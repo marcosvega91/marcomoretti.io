11e0248b713683ae82fac9d804c6e84b6e7dff11
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-01 09:14:35 +0200

    [Stepper] fix the stepper at the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import Stepper from './components/Stepper';
 import Step from './components/Step';
+
+const StepperWrapper = styled.div`
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Stepper orientation="horizontal" activeStep={1}>
-        <Step>Hello</Step>
-        <Step>Skill</Step>
-        <Step>Contact</Step>
-      </Stepper>
+      <StepperWrapper>
+        <Stepper orientation="horizontal" activeStep={1}>
+          <Step>Hello</Step>
+          <Step>Skill</Step>
+          <Step>Contact</Step>
+        </Stepper>
+      </StepperWrapper>
     </div>
   );
 }
